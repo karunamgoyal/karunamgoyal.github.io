aa431e13b84a883446a2db6ce893149f64b1805a
karunam goyal <karunam.goyal25@gmail.com>
2019-07-14 07:03:30 +0530

    initial commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/home/karunamgoyal/deployingportfolio/karunamgoyal.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
